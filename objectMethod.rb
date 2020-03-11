b67ef34a54006dade9102cdd502c1642bb284ca8
Nimesh Patel <nimesh.patel@ifuturz.com>
2020-03-11 16:38:25 +0530

    object methods

--- objectMethod.rb
+++ objectMethod.rb
@@ -3,5 +3,21 @@ class Student
     attr_accessor :id,:name,:gpa
     def initialize (id,name,gpa)
         @id = id
+        @name = name
+        @gpa = gpa
+    end
+
+    def is_honour
+        if gpa > 3.5
+            return true
+        end
+        return false
     end
 end
+
+
+student1 = Student.new(1,'john',2.6)
+student2 = Student.new(2,'kim',4.5)
+
+puts student1.is_honour
+puts student2.is_honour
